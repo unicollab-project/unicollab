bf397a9a15f8878061fb76378f5348e029f26e78
Vineet Motwani <bbba4309@gmail.com>
2024-04-23 19:28:44 +0530

    Debug banner removed

--- lib/main.dart
+++ lib/main.dart
@@ -79,6 +79,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'Unicollab',
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
